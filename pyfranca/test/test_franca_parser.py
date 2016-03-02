7d17c79cb2185693daf3226c1eb709e049d8fdf7
Kaloyan Tenchov <zayfod@gmail.com>
2016-03-02 13:56:14 -0500

    Initial parser test suite.

---
+++ pyfranca/test/test_franca_parser.py
@@ -0,0 +1,202 @@
+
+import unittest
+from pyfranca.franca_parser import Parser
+from pyfranca.ast import Package
+
+
+class BaseTestCase(unittest.TestCase):
+
+    def _parse(self, data):
+        parser = Parser()
+        package = parser.parse(data)
+        return package
+
+    def _assertParse(self, data):
+        package = self._parse(data)
+        self.assertIsInstance(package, Package)
+        return package
+
+    def _assertDoesNotParse(self, data):
+        package = self._parse(data)
+        self.assertIsNone(package)
+
+
+class TestParserNormal(BaseTestCase):
+    """Test normal usage of Parser."""
+
+    def test_empty_package(self):
+        package = self._assertParse("package P")
+        self.assertEqual(package.name, "P")
+        self.assertIsNone(package.file)
+        self.assertEqual(len(package.imports), 0)
+        self.assertEqual(len(package.typecollections), 0)
+        self.assertEqual(len(package.interfaces), 0)
+
+    def test_import_namespace(self):
+        package = self._assertParse("package P import NS from \"test.fidl\"")
+        self.assertEqual(package.name, "P")
+        self.assertIsNone(package.file)
+        self.assertEqual(len(package.imports), 1)
+        self.assertEqual(len(package.typecollections), 0)
+        self.assertEqual(len(package.interfaces), 0)
+        imp = package.imports[0]
+        self.assertEqual(imp.namespace, "NS")
+        self.assertEqual(imp.file, "test.fidl")
+
+    def test_import_model(self):
+        package = self._assertParse("package P import model \"test.fidl\"")
+        self.assertEqual(package.name, "P")
+        self.assertIsNone(package.file)
+        self.assertEqual(len(package.imports), 1)
+        self.assertEqual(len(package.typecollections), 0)
+        self.assertEqual(len(package.interfaces), 0)
+        imp = package.imports[0]
+        self.assertIsNone(imp.namespace)
+        self.assertEqual(imp.file, "test.fidl")
+
+    def test_import_two_namespaces(self):
+        package = self._assertParse("""
+            package P
+            import NS1 from \"test1.fidl\"
+            import NS2 from \"test2.fidl\"
+        """)
+        self.assertEqual(package.name, "P")
+        self.assertIsNone(package.file)
+        self.assertEqual(len(package.imports), 2)
+        self.assertEqual(len(package.typecollections), 0)
+        self.assertEqual(len(package.interfaces), 0)
+        imp = package.imports[0]
+        self.assertEqual(imp.namespace, "NS1")
+        self.assertEqual(imp.file, "test1.fidl")
+        imp = package.imports[1]
+        self.assertEqual(imp.namespace, "NS2")
+        self.assertEqual(imp.file, "test2.fidl")
+
+    def test_empty_typecollection(self):
+        package = self._assertParse("package P typeCollection TC {}")
+        self.assertEqual(package.name, "P")
+        self.assertIsNone(package.file)
+        self.assertEqual(len(package.imports), 0)
+        self.assertEqual(len(package.typecollections), 1)
+        self.assertEqual(len(package.interfaces), 0)
+        self.assertTrue("TC" in package.typecollections)
+        typecollection = package.typecollections["TC"]
+        self.assertEqual(typecollection.package, package)
+        self.assertEqual(typecollection.name, "TC")
+        self.assertListEqual(typecollection.flags, [])
+        self.assertIsNone(typecollection.version)
+        self.assertEqual(len(typecollection.typedefs), 0)
+        self.assertEqual(len(typecollection.enumerations), 0)
+        self.assertEqual(len(typecollection.structs), 0)
+        self.assertEqual(len(typecollection.arrays), 0)
+        self.assertEqual(len(typecollection.maps), 0)
+
+    def test_two_empty_typecollections(self):
+        package = self._assertParse("""
+            package P
+            typeCollection TC1 {}
+            typeCollection TC2 {}
+        """)
+        self.assertEqual(package.name, "P")
+        self.assertIsNone(package.file)
+        self.assertEqual(len(package.imports), 0)
+        self.assertEqual(len(package.typecollections), 2)
+        self.assertEqual(len(package.interfaces), 0)
+        self.assertTrue("TC1" in package.typecollections)
+        typecollection = package.typecollections["TC1"]
+        self.assertEqual(typecollection.package, package)
+        self.assertEqual(typecollection.name, "TC1")
+        self.assertListEqual(typecollection.flags, [])
+        self.assertIsNone(typecollection.version)
+        self.assertEqual(len(typecollection.typedefs), 0)
+        self.assertEqual(len(typecollection.enumerations), 0)
+        self.assertEqual(len(typecollection.structs), 0)
+        self.assertEqual(len(typecollection.arrays), 0)
+        self.assertEqual(len(typecollection.maps), 0)
+        self.assertTrue("TC2" in package.typecollections)
+        typecollection = package.typecollections["TC2"]
+        self.assertEqual(typecollection.package, package)
+        self.assertEqual(typecollection.name, "TC2")
+        self.assertListEqual(typecollection.flags, [])
+        self.assertIsNone(typecollection.version)
+        self.assertEqual(len(typecollection.typedefs), 0)
+        self.assertEqual(len(typecollection.enumerations), 0)
+        self.assertEqual(len(typecollection.structs), 0)
+        self.assertEqual(len(typecollection.arrays), 0)
+        self.assertEqual(len(typecollection.maps), 0)
+
+    def test_empty_interface(self):
+        package = self._assertParse("package P interface I {}")
+        self.assertEqual(package.name, "P")
+        self.assertIsNone(package.file)
+        self.assertEqual(len(package.imports), 0)
+        self.assertEqual(len(package.typecollections), 0)
+        self.assertEqual(len(package.interfaces), 1)
+        self.assertTrue("I" in package.interfaces)
+        interface = package.interfaces["I"]
+        self.assertEqual(interface.package, package)
+        self.assertEqual(interface.name, "I")
+        self.assertListEqual(interface.flags, [])
+        self.assertIsNone(interface.version)
+        self.assertEqual(len(interface.typedefs), 0)
+        self.assertEqual(len(interface.enumerations), 0)
+        self.assertEqual(len(interface.structs), 0)
+        self.assertEqual(len(interface.arrays), 0)
+        self.assertEqual(len(interface.maps), 0)
+        self.assertEqual(len(interface.attributes), 0)
+        self.assertEqual(len(interface.methods), 0)
+        self.assertEqual(len(interface.broadcasts), 0)
+
+    def test_two_empty_interfaces(self):
+        package = self._assertParse("""
+            package P
+            interface I1 {}
+            interface I2 {}
+        """)
+        self.assertEqual(package.name, "P")
+        self.assertIsNone(package.file)
+        self.assertEqual(len(package.imports), 0)
+        self.assertEqual(len(package.typecollections), 0)
+        self.assertEqual(len(package.interfaces), 2)
+        self.assertTrue("I1" in package.interfaces)
+        interface = package.interfaces["I1"]
+        self.assertEqual(interface.package, package)
+        self.assertEqual(interface.name, "I1")
+        self.assertListEqual(interface.flags, [])
+        self.assertIsNone(interface.version)
+        self.assertEqual(len(interface.typedefs), 0)
+        self.assertEqual(len(interface.enumerations), 0)
+        self.assertEqual(len(interface.structs), 0)
+        self.assertEqual(len(interface.arrays), 0)
+        self.assertEqual(len(interface.maps), 0)
+        self.assertEqual(len(interface.attributes), 0)
+        self.assertEqual(len(interface.methods), 0)
+        self.assertEqual(len(interface.broadcasts), 0)
+        self.assertTrue("I2" in package.interfaces)
+        interface = package.interfaces["I2"]
+        self.assertEqual(interface.package, package)
+        self.assertEqual(interface.name, "I2")
+        self.assertListEqual(interface.flags, [])
+        self.assertIsNone(interface.version)
+        self.assertEqual(len(interface.typedefs), 0)
+        self.assertEqual(len(interface.enumerations), 0)
+        self.assertEqual(len(interface.structs), 0)
+        self.assertEqual(len(interface.arrays), 0)
+        self.assertEqual(len(interface.maps), 0)
+        self.assertEqual(len(interface.attributes), 0)
+        self.assertEqual(len(interface.methods), 0)
+        self.assertEqual(len(interface.broadcasts), 0)
+
+
+class TestParserError(BaseTestCase):
+    """Test passing invalid input to Parser."""
+
+    @unittest.skip("Bug in syntax error handling?")
+    def test_empty(self):
+        """A package statement is expected as a minimum."""
+        self._assertDoesNotParse("")
+
+    @unittest.skip("Bug in syntax error handling?")
+    def test_garbage(self):
+        """Invalid input."""
+        self._assertDoesNotParse("%!@#")
