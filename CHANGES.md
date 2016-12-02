3c82ac4505b08f22f96a7085a72b5850ef229266
Kaloyan Tenchov <zayfod@gmail.com>
2016-12-01 23:35:27 -0500

    Updated CHANGES.md and setup.py for v0.1.1 .

--- CHANGES.md
+++ CHANGES.md
@@ -2,6 +2,14 @@
 Revision History
 ================
 
+v0.1.1 (Dec 1, 2016)
+--------------------
+- Fix for structure field parsing (thanks to jay-o-stotch).
+- Replaced distutils with setuptools.
+- Simplified importing.
+- More unit tests.
+- Integrated Coveralls and Scrutinizer.
+
 v0.1.0 (Mar 3, 2016)
 --------------------
 - Initial release.
--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ from setuptools import setup
 setup(
     name="pyfranca",
     packages=["pyfranca"],
-    version="0.2.0",
+    version="0.1.1",
     description="Python parser and tools for working with the Franca "
                 "interface definition language.",
     author="Kaloyan Tenchov",
     author_email="zayfod@gmail.com",
     url="http://github.com/zayfod/pyfranca",
+    download_url="https://github.com/zayfod/pyfranca/archive/0.1.1.zip",
     license="MIT",
     platforms="Python 2.6 and later.",
     keywords=["franca", "idl", "fidl", "parser"],
