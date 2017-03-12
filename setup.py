4fea94d249b6018a4d1b6c8608330b5788b57c28
Kaloyan Tenchov <zayfod@gmail.com>
2017-03-11 20:48:52 -0500

    Reworked the way version is retrieved in setup.py so that the package does not have to be imported.

--- setup.py
+++ setup.py
@@ -1,17 +1,32 @@
 
-from setuptools import setup
-from pyfranca import __version__
+import os
+import re
+from setuptools import setup, find_packages
+
+
+def read_package_variable(key):
+    fspec = os.path.join("pyfranca", "__init__.py")
+    with open(fspec) as f:
+        for line in f:
+            m = re.match(r"(\S+)\s*=\s*[\"']?(.+?)[\"']?\s*$", line)
+            if m and key == m.group(1):
+                return m.group(2)
+    return None
+
+
+version = read_package_variable("__version__")
 
 setup(
     name="pyfranca",
-    packages=["pyfranca"],
-    version=__version__,
+    packages=find_packages(),
+    version=version,
     description="Python parser and tools for working with the Franca "
                 "interface definition language.",
     author="Kaloyan Tenchov",
     author_email="zayfod@gmail.com",
     url="http://github.com/zayfod/pyfranca",
-    download_url="https://github.com/zayfod/pyfranca/archive/0.2.0.zip",
+    download_url="https://github.com/zayfod/pyfranca/archive/{}.zip".format(
+        version),
     license="MIT",
     platforms="Python 2.7 and later.",
     keywords=["franca", "franca-idl", "idl", "fidl", "parsing"],
