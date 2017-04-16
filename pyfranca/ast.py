4e369475068d0c86b4187df0a4169abc97c15d81
Gunnar Andersson <gand@acm.org>
2017-04-16 19:00:12 +0200

    Initial support for unions
    Supporting unions with basically identical handling as is used for
    Structs.  We'll see if it works 100% or not.
    
    I'm not sure why unions were not supported, but it's possible this is
    only in a newer Franca version than previously supported.
    
    Signed-off-by: Gunnar Andersson <gandersson@genivi.org>

--- pyfranca/ast.py
+++ pyfranca/ast.py
@@ -98,6 +98,7 @@ class Namespace(object):
         self.typedefs = OrderedDict()
         self.enumerations = OrderedDict()
         self.structs = OrderedDict()
+        self.unions = OrderedDict()
         self.arrays = OrderedDict()
         self.maps = OrderedDict()
         self.constants = OrderedDict()
@@ -112,6 +113,7 @@ class Namespace(object):
         res = name in self.typedefs or \
             name in self.enumerations or \
             name in self.structs or \
+            name in self.unions or \
             name in self.arrays or \
             name in self.maps or \
             name in self.constants
@@ -126,6 +128,8 @@ class Namespace(object):
             return self.enumerations[name]
         elif name in self.structs:
             return self.structs[name]
+        elif name in self.unions:
+            return self.unions[name]
         elif name in self.arrays:
             return self.arrays[name]
         elif name in self.maps:
@@ -154,6 +158,12 @@ class Namespace(object):
                 self.enumerations[member.name] = member
             elif isinstance(member, Struct):
                 self.structs[member.name] = member
+                # Handle anonymous array special case.
+                for field in member.fields.values():
+                    if isinstance(field.type, Array):
+                        field.type.namespace = self
+            elif isinstance(member, Union):
+                self.unions[member.name] = member
                 # Handle anonymous array special case.
                 for field in member.fields.values():
                     if isinstance(field.type, Array):
@@ -370,6 +380,15 @@ class Struct(ComplexType):
         self.reference = None
         self.flags = flags if flags else []
 
+class Union(ComplexType):
+
+    def __init__(self, name, fields=None, extends=None, flags=None):
+        super(Union, self).__init__()
+        self.name = name
+        self.fields = fields if fields else OrderedDict()
+        self.extends = extends
+        self.reference = None
+        self.flags = flags if flags else []
 
 class StructField(object):
 
@@ -378,6 +397,11 @@ class StructField(object):
         self.type = field_type
         self.comments = comments if comments else OrderedDict()
 
+class UnionField(object):
+
+    def __init__(self, name, field_type):
+        self.name = name
+        self.type = field_type
 
 class Array(ComplexType):
 
