dc6a0c143e326a86bea6097f49262c43f11b00ef
Gunnar Andersson <gand@acm.org>
2017-04-16 19:03:13 +0200

    Workaround for Franca "error" definitions that use enums
    I can't really tell if this is a legitimate workaround or not - it just
    made me get one step further.
    
    Signed-off-by: Gunnar Andersson <gandersson@genivi.org>

--- pyfranca/franca_processor.py
+++ pyfranca/franca_processor.py
@@ -231,7 +231,12 @@ class Processor:
                 self._update_type_references(name.namespace, arg.type)
             if isinstance(name.errors, OrderedDict):
                 for arg in name.errors.values():
-                    self._update_type_references(name.namespace, arg.type)
+                    # I end up here also when it's an Enumeration so it
+                    # fails on arg.type.  Why?  Trying to workaround
+                    try:
+                        self._update_type_references(name.namespace, arg.type)
+                    except:
+                        pass
             elif isinstance(name.errors, ast.Reference):
                 # Errors can be a reference to an enumeration
                 self._update_type_references(name.namespace, name.errors)
