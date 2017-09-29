73654ef5f3172cfe19d39ec5fee8aaab98186346
Kaloyan Tenchov <zayfod@gmail.com>
2017-09-29 11:10:06 -0400

    Add support for structured comments to fidl_dump.py .

--- tools/fidl_dump.py
+++ tools/fidl_dump.py
@@ -5,27 +5,37 @@ from pyfranca import Processor, LexerException, ParserException, \
     ProcessorException
 
 
+def dump_comments(item, prefix):
+    for key, value in item.comments.items():
+        print (prefix + key + ": " + value)
+
+
 def dump_namespace(namespace):
     if namespace.typedefs:
         print("\t\tTypedefs:")
         for item in namespace.typedefs.values():
             print("\t\t- {} is {}".format(item.name, item.type.name))
+            dump_comments(item, "\t\t\t")
     if namespace.enumerations:
         print("\t\tEnumerations:")
         for item in namespace.enumerations.values():
             print("\t\t- {}".format(item.name))
+            dump_comments(item, "\t\t\t")
     if namespace.structs:
         print("\t\tStructs:")
         for item in namespace.structs.values():
             print("\t\t- {}".format(item.name))
+            dump_comments(item, "\t\t\t")
     if namespace.arrays:
         print("\t\tArrays:")
         for item in namespace.arrays.values():
             print("\t\t- {}".format(item.name))
+            dump_comments(item, "\t\t\t")
     if namespace.maps:
         print("\t\tMaps:")
         for item in namespace.maps.values():
             print("\t\t- {}".format(item.name))
+            dump_comments(item, "\t\t\t")
 
 
 def dump_interface(interface):
@@ -33,19 +43,23 @@ def dump_interface(interface):
         print("\t\tAttributes:")
         for item in interface.attributes.values():
             print("\t\t- {}".format(item.name))
+            dump_comments(item, "\t\t\t")
     if interface.methods:
         print("\t\tMethods:")
         for item in interface.methods.values():
             print("\t\t- {}()".format(item.name))
+            dump_comments(item, "\t\t\t")
     if interface.broadcasts:
         print("\t\tBroadcasts:")
         for item in interface.broadcasts.values():
             print("\t\t- {}".format(item.name))
+            dump_comments(item, "\t\t\t")
     dump_namespace(interface)
 
 
 def dump_package(package):
     print("- {} ({})".format(package.name, str.join(", ", package.files)))
+    dump_comments(package, "\t")
     if package.imports:
         print("\tImports:")
         for imp in package.imports:
@@ -58,6 +72,7 @@ def dump_package(package):
             else:
                 version_str = ""
             print("\t- {}{}".format(interface.name, version_str))
+            dump_comments(interface, "\t\t")
             dump_interface(interface)
     if package.typecollections:
         print("\tType collections:")
@@ -67,6 +82,7 @@ def dump_package(package):
             else:
                 version_str = ""
             print("\t- {}{}".format(typecollection.name, version_str))
+            dump_comments(typecollection, "\t\t")
             dump_namespace(typecollection)
 
 
